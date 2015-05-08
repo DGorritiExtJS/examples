4574362217fa01366f1648429a67b830d9a0e7a5
Gorriti13 <gorriti_13@hotmail.com>
2015-05-08 10:25:52 +0200

    finish FormGrid Add/Edit/Delete

--- js/FormGrid.js
+++ js/FormGrid.js
@@ -1,6 +1,7 @@
 var users = [];
 var usersArrayStore;
-var userID = 0;
+var userID = 0, selectedUserID = -1, gridSelectedIndex = -1;
+var usersGrid;
 
 Ext.application({
 	name   : 'MyApp',
@@ -21,7 +22,10 @@ Ext.application({
 function loadDefaultUsers(){
 
 	for (i = 0; i < 10; i++){
-		var user = createUser('David' + i,26 + i,'email@hotmail.com');
+		var user = createUser();
+		user.setName('David' + i);
+		user.setAge(26 + i);
+		user.setEmail('email@hotmail.com');
 		user.save();
 	};
 
@@ -34,24 +38,58 @@ function defineUser(){
 		{name: 'id', type: 'int'},
 		{name: 'name',  type: 'string'},
 		{name: 'age',   type: 'int', convert: null},
-		{name: 'email', type: 'string'}
+		{name: 'email', type: 'string'},
+		{name: 'index', type: 'int'}
 		],
-		changeName: function(newName) {
+		setName: function(newName) {
 			this.set('name', newName);
 		},
+		setAge: function(newName) {
+			this.set('age', newName);
+		},
+		setEmail: function(newName) {
+			this.set('email', newName);
+		},
 		save: function(){
-			users.push(this);
+
+			var index = this.get('index');
+
+			if (index > -1) {
+
+				users.splice(index,1,this);
+
+			}else{
+
+				userID++;
+				this.set('id',userID);
+				users.push(this);
+
+			}
+		},
+		getUser: function(id){
+			var  i = 0, found = false;
+			while(i < users.length && found == false){
+				if (users[i].get('id') === id){
+					this.setName(users[i].get('name'));
+					this.setAge(users[i].get('age'));
+					this.setEmail(users[i].get('email'));
+					this.set('id',users[i].get('id'));
+					this.set('index',i);
+					found = true;
+				};
+				i++;
+			}
 		}
 	});
 };
 
-function createUser(name,age,email){
-	userID++;
+function createUser(){
 	var user = Ext.create('User', {
-		id   : userID,
-		name : name,
-		age  : age,
-		email: email
+		index: -1,
+		id   : -1,
+		name : '',
+		age  : 0,
+		email: ''
 	});
 	return user;
 };
@@ -87,6 +125,7 @@ function createPanel(){
         vtype: 'email',  // requires value to be a valid email address format
         allowBlank: false
     }, 
+    createNewButton(),
     createSaveButton(),
     createGrid()
     ]
@@ -98,14 +137,45 @@ function createPanel(){
 
 function saveInfo(){
 
+	var user = createUser();
 	var name = Ext.getCmp('nameTextField').getValue();
 	var age = Ext.getCmp('ageTextField').getValue();
 	var email = Ext.getCmp('emailTextField').getValue();
 
-	var user = createUser(name,age,email);
-	user.save();
+	if (selectedUserID == -1){
 
-	usersArrayStore.reload();
+		user.setName(name);
+		user.setAge(age);
+		user.setEmail(email);
+		user.save();
+
+		usersArrayStore.reload();
+		resetForm();
+
+	}else{
+
+		user.getUser(selectedUserID);
+
+		user.setName(name);
+		user.setAge(age);
+		user.setEmail(email);
+
+		user.save();
+		usersArrayStore.reload();
+		usersGrid.getSelectionModel().select(gridSelectedIndex)
+
+	}
+
+}
+
+function createNewButton(){
+
+	var button = Ext.create('Ext.Button',{
+		text: 'New User',
+		handler: newUser
+	});
+
+	return button;
 
 }
 
@@ -121,56 +191,51 @@ function createSaveButton(){
 
 }
 
-function createDeleteButton(){
+function newUser(){
 
-	var button = Ext.create('Ext.Button',{
-		text: 'Delete Selection',
-		handler: deleteSelection
-	});
-
-	return button;
+	selectedUserID = -1;
+	gridSelectedIndex = -1;
+	resetForm();
+	usersGrid.getSelectionModel().deselectAll();
 
 }
 
 function deleteSelection(grid, rowIndex, colIndex){
 
 	var rec = grid.getStore().getAt(rowIndex);
-    var id = rec.get('id');
-    //var index = arrayObjectIndexOf(id);
-    //alert(index);
-    //BOTH ARE WORKING
-	//grid.getStore().remove(rec);
-	grid.getStore().removeAt(rowIndex);
-	//alert("Delete " + rec.get('name'));
+	var id = rec.get('id');
+	var index = arrayObjectIndexOf(id)
+	if (index > -1){
+		users.splice(index,1);
+	};
 
+    //ALL ARE WORKING
+    usersArrayStore.reload();
+	//grid.getStore().remove(rec);
+	//grid.getStore().removeAt(rowIndex);
+	resetForm();
+
+}
+
+function resetForm(){
+	Ext.getCmp('nameTextField').setValue('');
+	Ext.getCmp('ageTextField').setValue('');
+	Ext.getCmp('emailTextField').setValue('');
+}
+
+function drawInfo(dv, record, item, index, e){
+	gridSelectedIndex = index;
+	selectedUserID = record.get('id')
+	Ext.getCmp('nameTextField').setValue(record.get('name'));
+	Ext.getCmp('ageTextField').setValue(record.get('age'));
+	Ext.getCmp('emailTextField').setValue(record.get('email'));
 }
 
 function arrayObjectIndexOf(searchTerm) {
-    for(var i = 0; i < myArray.length; i++) {
-        if (users[i].get('id') === searchTerm) return i;
-    }
-    return -1;
-}
-
-function showUsersInfo(){
-
-	var info = "";
-
-	users.forEach(function(user){
-
-		info += user.get('name') + ' - ' + user.get('age') + ' - ' 
-		+ user.get('email') + '\n';
-
-	});
-
-	Ext.each(usersArrayStore.getRange(), function (item, idx, a) {
-		for (var i in item.data) {
-			info += item.data[i] + '\n';
-		}
-	});
-
-	alert(info);
-
+	for(var i = 0; i < users.length; i++) {
+		if (users[i].get('id') === searchTerm) return i;
+	}
+	return -1;
 }
 
 function createUsersArrayStore(){
@@ -184,11 +249,11 @@ function createUsersArrayStore(){
 
 function createGrid(){
 
-	var usersGrid = Ext.create('Ext.grid.Panel', {
+	usersGrid = Ext.create('Ext.grid.Panel', {
 		store: usersArrayStore,
 		stateful: true,
 		collapsible: true,
-		multiSelect: true,
+		multiSelect: false,
 		stateId: 'usersGrid',
 		height: 250,
 		title: 'Array Grid',
@@ -230,9 +295,12 @@ function createGrid(){
                 tooltip: 'Delete',
                 handler: deleteSelection
             }]
-        }]
+        }],
+        listeners:{
+        	itemclick: drawInfo
+        }
     });
 
-return usersGrid;
+	return usersGrid;
 
 }
